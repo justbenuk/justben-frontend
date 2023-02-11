ae22f2d5a1b5b6b44779efea862be961d2413050
justbenuk <justbenuk@gmail.com>
2023-02-11 00:38:07 +0000

    updated api url

--- config/index.js
+++ config/index.js
@@ -1,1 +1,1 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:1337'
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://stingray-app-gukxi.ondigitalocean.app/cp/'
